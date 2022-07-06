4118539de4504c6e20a95ba90d5fea5215bf6055
Alexandre Bourret <alexandre.bourret@dataiku.com>
2022-07-06 16:55:41 +0200

    Adding unit tests for is_child_attribute_path

--- tests/python/unit/test_osisoft_client.py
+++ tests/python/unit/test_osisoft_client.py
@@ -1,4 +1,5 @@
 from osisoft_client import format_output_row
+from osisoft_plugin_common import is_child_attribute_path
 import pytest
 
 
@@ -13,7 +14,21 @@ class TestCommonMethods:
                     ]}
                 ]
             }
+        self.path_to_child_attribute = "\\\\server\\database\\element1\\element2|attribute|child"
+        self.path_to_attribute = "\\\\server\\database\\element1\\element2|attribute"
+        self.path_to_attribute_with_pipe = "\\\\server\\database\\element1|withpipe|withsecondpipe\\element2|attribute"
+        self.path_to_element = "\\\\server\\database\\element1\\element2"
 
     def test_format_output_row(self):
         formated_rows = list(format_output_row(self.rows_to_format))
         assert formated_rows[0] == {'a': 1, 'c': 111, 'b': 11, 'v': 1}
+
+    def test_is_child_attribute_path(self):
+        is_child_attribute = is_child_attribute_path(self.path_to_attribute)
+        assert not is_child_attribute
+        is_child_attribute = is_child_attribute_path(self.path_to_attribute_with_pipe)
+        assert not is_child_attribute
+        is_child_attribute = is_child_attribute_path(self.path_to_child_attribute)
+        assert is_child_attribute
+        is_child_attribute = is_child_attribute_path(self.path_to_element)
+        assert not is_child_attribute
