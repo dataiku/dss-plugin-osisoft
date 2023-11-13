db02346860857eed7bfdb4a5d539888dc2c196e8
Alexandre Bourret <alexandre.bourret@dataiku.com>
2023-11-13 16:09:23 +0100

    Update beta

--- python-lib/osisoft_constants.py
+++ python-lib/osisoft_constants.py
@@ -403,7 +403,7 @@ class OSIsoftConstants(object):
         "Security": "{base_url}/eventframes/{webid}/security",
         "SecurityEntries": "{base_url}/eventframes/{webid}/securityentries"
     }
-    PLUGIN_VERSION = "1.2.0-beta.8"
+    PLUGIN_VERSION = "1.2.0-beta.10"
     VALUE_COLUMN_SUFFIX = "_val"
     WEB_API_PATH = "piwebapi"
     WRITE_HEADERS = {'X-Requested-With': 'XmlHttpRequest'}
