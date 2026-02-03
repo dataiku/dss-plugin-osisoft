533a77375a0669b348d3e6527f5211f8ff2ce207
JaneBellaiche <jane.bellaiche@dataiku.com>
2026-02-03 21:02:08 +0100

    *use js better practices *formatting

--- js/pi-system_treecontroller.js
+++ js/pi-system_treecontroller.js
@@ -1,84 +1,84 @@
 var app = angular.module('piSystemTreeApp.module', []);
 
-app.service('TreeDataService', function() {
+app.service('TreeDataService', function () {
   // This will store the shared tree data
   this.treeData = [];
 
   // Optional: helper methods
-  this.setTreeData = function(data) {
+  this.setTreeData = function (data) {
     this.treeData = data;
   };
 
-  this.getTreeData = function() {
+  this.getTreeData = function () {
     return this.treeData;
   };
 });
 
 app.controller('AfExplorerFormCtrl', [
-  '$scope', 
-  '$stateParams', 
-  'CodeMirrorSettingService', 
-  'TreeDataService', 
-  function($scope, $stateParams, CodeMirrorSettingService, TreeDataService) {
-    
+  '$scope',
+  '$stateParams',
+  'CodeMirrorSettingService',
+  'TreeDataService',
+  function ($scope, $stateParams, CodeMirrorSettingService, TreeDataService) {
+
     $scope.paramDesc = {
       'parameterSetId': 'basic-auth',
       'mandatory': true
     };
-    
+
     $scope.treeData = TreeDataService.getTreeData();
     $scope.config.attributeList = $scope.config.attributeList || [];
     $scope.config.selectedAttributes = $scope.config.selectedAttributes || [];
-    
+
     $scope.editorOptions = CodeMirrorSettingService.get("text/plain");
 
-    $scope.init = function() {
-        DataikuAPI.plugins.listAccessiblePresets('pi-system', $stateParams.projectKey, 'basic-auth').success(function (data) {
-            $scope.inlineParams = data.inlineParams;
-            $scope.inlinePluginParams = data.inlinePluginParams;
-            $scope.accessiblePresets = [];
-            if (data.definableInline) {
-                $scope.accessiblePresets.push({
-                    name:"INLINE",
-                    label:"Manually defined", usable:true,
-                    description: "Define values for these parameters"
-                });
-            }
-            data.presets.forEach(function(p) {
-                $scope.accessiblePresets.push({name:"PRESET " + p.name, label:p.name, usable:p.usable, description:p.description});
-            });
-            $scope.accessibleParameterSetDescriptions = $scope.accessiblePresets.map(function(p) { 
-                return p.description || '<em>No description</em>';
-            });
-        }).error(setErrorInScope.bind($scope.errorScope));
-        if($scope.authConfigured() === true){
-          $scope.authSectionVisible = false;
-          $scope.showTreeData = true;
+    $scope.init = function () {
+      DataikuAPI.plugins.listAccessiblePresets('pi-system', $stateParams.projectKey, 'basic-auth').success(function (data) {
+        $scope.inlineParams = data.inlineParams;
+        $scope.inlinePluginParams = data.inlinePluginParams;
+        $scope.accessiblePresets = [];
+        if (data.definableInline) {
+          $scope.accessiblePresets.push({
+            name: "INLINE",
+            label: "Manually defined", usable: true,
+            description: "Define values for these parameters"
+          });
         }
+        data.presets.forEach(function (p) {
+          $scope.accessiblePresets.push({ name: "PRESET " + p.name, label: p.name, usable: p.usable, description: p.description });
+        });
+        $scope.accessibleParameterSetDescriptions = $scope.accessiblePresets.map(function (p) {
+          return p.description || '<em>No description</em>';
+        });
+      }).error(setErrorInScope.bind($scope.errorScope));
+      if ($scope.authConfigured() === true) {
+        $scope.authSectionVisible = false;
+        $scope.showTreeData = true;
+      }
     };
-    
-    $scope.getServers = function(){
-      $scope.callPythonDo({parameterName: "server_name"}).then(function(data){
+
+    $scope.getServers = function () {
+      $scope.callPythonDo({ parameterName: "server_name" }).then(function (data) {
         $scope.server_name = data.choices;
       });
     };
-    $scope.getDatabases = function() {
-      $scope.callPythonDo({parameterName: "database_name"}).then(function(data){
+    $scope.getDatabases = function () {
+      $scope.callPythonDo({ parameterName: "database_name" }).then(function (data) {
         $scope.database_name = data.choices;
       });
     };
 
     $scope.authSectionVisible = $scope.authSectionVisible || true;
 
-    $scope.toggleAuthSection = function() {
+    $scope.toggleAuthSection = function () {
       $scope.authSectionVisible = !$scope.authSectionVisible;
     };
 
-    $scope.authConfigured = function(){
+    $scope.authConfigured = function () {
       console.log('authConfigured check');
-      return $scope.hasPreset() && $scope.config.database_name && $scope.config.database_name.length > 0 && $scope.config.server_name && $scope.config.server_name.length > 0 ;
+      return $scope.hasPreset() && $scope.config.database_name && $scope.config.database_name.length > 0 && $scope.config.server_name && $scope.config.server_name.length > 0;
     }
-    $scope.explore = function() {
+    $scope.explore = function () {
       console.log("coucou");
       if ($scope.authConfigured()) {
         console.log("here");
@@ -86,178 +86,184 @@ app.controller('AfExplorerFormCtrl', [
       }
     };
 
-    $scope.hasPreset = function() {
-      return $scope.config.credentials && $scope.config.credentials.mode && $scope.config.credentials.mode!=='NONE' && $scope.config.credentials.name
+    $scope.hasPreset = function () {
+      return $scope.config.credentials && $scope.config.credentials.mode && $scope.config.credentials.mode !== 'NONE' && $scope.config.credentials.name
     }
-      
-    $scope.initializeTree = function(){
-        console.log("initialization: ");
-        console.log($scope.config.treeData);
-        if (!$scope.config.treeData || $scope.config.treeData.length === 0){
-      $scope.callPythonDo({method: "get_children_from_db", parent: $scope.config.database_name}).then(function(data){
+
+    $scope.initializeTree = function () {
+      console.log("initialization: ");
+      console.log($scope.config.treeData);
+      if (!$scope.config.treeData || $scope.config.treeData.length === 0) {
+        $scope.callPythonDo({ method: "get_children_from_db", parent: $scope.config.database_name }).then(function (data) {
           console.log("ALX:data2=" + JSON.stringify(data));
           TreeDataService.setTreeData(data.choices);
-           $scope.config.treeData =  TreeDataService.getTreeData();
-      });
-        }
-    };
-      
-        $scope.getChildrenFromDB = function(item){
-    console.log("ALX:gcfd:" + JSON.stringify(item));
-    return $scope.callPythonDo({ method: "get_children_from_db", parent: item })
-      .then(function (data) {
-        console.log("ALX:data1=" + JSON.stringify(data));
-        item.children = data.choices;
-        item.children.forEach(child => {
-          child.expanded = false;
+          $scope.config.treeData = TreeDataService.getTreeData();
         });
-        console.log(item);
-        return item;
-      });
-    } 
+      }
+    };
 
-    $scope.getTemplatesFromDB = function() {
-      $scope.callPythonDo({method: "get_templates_from_db"}).then(function(data){
+    $scope.getChildrenFromDB = function (item) {
+      console.log("ALX:gcfd:" + JSON.stringify(item));
+      return $scope.callPythonDo({ method: "get_children_from_db", parent: item })
+        .then(function (data) {
+          console.log("ALX:data1=" + JSON.stringify(data));
+          item.children = data.choices;
+          item.children.forEach(child => {
+            child.expanded = false;
+          });
+          console.log(item);
+          return item;
+        });
+    }
+
+    $scope.getTemplatesFromDB = function () {
+      $scope.callPythonDo({ method: "get_templates_from_db" }).then(function (data) {
         $scope.config.templates = data.choices;
       });
     }
 
-    $scope.getCategoriesFromDB = function(){
+    $scope.getCategoriesFromDB = function () {
       $scope.config.attribute_categories = [];
       $scope.config.element_categories = [];
-      $scope.callPythonDo({method: "get_attribute_categories_from_db"}).then(function(data){
+      $scope.callPythonDo({ method: "get_attribute_categories_from_db" }).then(function (data) {
         $scope.config.attribute_categories = data.choices;
       });
-      $scope.callPythonDo({method: "get_element_categories_from_db"}).then(function(data){
+      $scope.callPythonDo({ method: "get_element_categories_from_db" }).then(function (data) {
         $scope.config.element_categories = data.choices;
       });
     }
 
-  // Toggle récursif des checkboxes
-  $scope.toggleChildren = function(node) {
-    console.log("ALX:tc:" + JSON.stringify(node));
+    // Toggle récursif des checkboxes
+    $scope.toggleChildren = function (node) {
+      console.log("ALX:tc:" + JSON.stringify(node));
       node.expanded = !node.expanded;
       $scope.getChildrenFromDB(node);
-    if (node.children && node.children.length) {
-      node.children.forEach(function(child) {
-        child.expanded = !child.expanded;
-        $scope.getChildrenFromDB(child);
-      });
-    }
-      
-  };
+      if (node.children && node.children.length) {
+        node.children.forEach(function (child) {
+          child.expanded = !child.expanded;
+          $scope.getChildrenFromDB(child);
+        });
+      }
 
-  $scope.doSearch = function(element_name, attribute_name){
-      $scope.callPythonDo({method: "do_search", element_name: element_name, attribute_name: attribute_name, root_tree: $scope.config.treeData}).then(
-        function(data){
+    };
+
+    $scope.doSearch = function (element_name, attribute_name) {
+      $scope.callPythonDo({ method: "do_search", element_name: element_name, attribute_name: attribute_name, root_tree: $scope.config.treeData }).then(
+        function (data) {
           TreeDataService.setTreeData(data.choices);
           $scope.config.treeData = TreeDataService.getTreeData();
           console.log("ALX:", JSON.stringify($scope.config.treeData));
           //todo: correct fix is to not have checked:true in the backend in the first place
-          $scope.config.attributeList = data.attributes.map(el=>({...el, checked:false})); 
+          $scope.config.attributeList = data.attributes.map(el => ({ ...el, checked: false }));
           $scope.config.selectedAttributes = [];
         }
       );
     };
 
-  $scope.updateAttributeToOutput = function (attribute) {
-    if (attribute.checked && $scope.config.selectedAttributes.includes(attribute)) {
-      $scope.config.selectedAttributes = $scope.config.selectedAttributes.filter(attr => attr.path !== attribute.path);
-    }
-    else {
-      console.log("Adding attribute to output:", attribute);
-
-      if (!$scope.config || !$scope.config.attributeList || $scope.config.selectedAttributes.includes(attribute)) {
-        return;
+    $scope.updateAttributeToOutput = function (attribute) {
+      if (attribute.checked && $scope.config.selectedAttributes.includes(attribute)) {
+        $scope.config.selectedAttributes = $scope.config.selectedAttributes.filter(attr => attr.path !== attribute.path);
       }
-      const attrInConfig = $scope.config.attributeList.find(attr => attr.path === attribute.path);
+      else {
+        console.log("Adding attribute to output:", attribute);
 
-      if (attrInConfig) {
-        $scope.config.selectedAttributes.push(attribute);
-        attrInConfig.checked = true;
-      } else {
-        console.warn("Attribute not found in config:", attribute.path);
-      }
-    }
-  };
-
-
-$scope.displayAttributes = function(node) {
-
-    if (!node.children || node.children.length === 0) {
-        $scope.getChildrenFromDB(node).then(newNode => {
-        processNode(newNode);
-        });
-    } else {
-        processNode(node);
-    };
-  }
-
-function processNode(node) {
-    if (node.title !== $scope.config.element_name) {
-      $scope.config.element_name = "";
-    }
-    $scope.config.attributeList =  [];
-    $scope.config.selectedAttributes =  [];
-    node.children.forEach(child => {
-        if (child.type === "attribute") {
-            $scope.config.attributeList.push({
-                "title": child.title,
-                "path": child.path
-            });
+        if (!$scope.config || !$scope.config.attributeList || $scope.config.selectedAttributes.includes(attribute)) {
+          return;
         }
-    });
-}
+        const attrInConfig = $scope.config.attributeList.find(attr => attr.path === attribute.path);
+
+        if (attrInConfig) {
+          $scope.config.selectedAttributes.push(attribute);
+          attrInConfig.checked = true;
+        } else {
+          console.warn("Attribute not found in config:", attribute.path);
+        }
+      }
+    };
 
 
-}]);
+    $scope.displayAttributes = function (node) {
 
-app.directive('treeNode', function() {
+      if (!node.children || node.children.length === 0) {
+        $scope.getChildrenFromDB(node).then(newNode => {
+          processNode(newNode);
+        });
+      } else {
+        processNode(node);
+      };
+    }
+
+    function processNode(node) {
+      if (node.title !== $scope.config.element_name) {
+        $scope.config.element_name = "";
+      }
+      $scope.config.attributeList = [];
+      $scope.config.selectedAttributes = [];
+      node.children.forEach(child => {
+        if (child.type === "attribute") {
+          $scope.config.attributeList.push({
+            "title": child.title,
+            "path": child.path
+          });
+        }
+      });
+    }
+
+
+  }]);
+
+app.directive('treeNode', function () {
   return {
     restrict: 'E',
-    scope: { node: '=' },
+    scope: {
+      node: '=',                  // mutable object (tree structure changes)
+      getChildrenFromDB: '<',     // function reference
+      displayAttributes: '<',     // function reference
+      config: '<'                 // read/write nested properties only
+    },
+
     template: `
       <div style="display: flex; align-items: center; gap: 6px;">
-        
-        <span ng-click="$event.stopPropagation();toggleExpand(node)"
+
+        <span ng-click="$event.stopPropagation(); toggleExpand(node)"
               style="cursor: pointer;">
           <strong ng-if="node.expanded">▼</strong>
           <strong ng-if="!node.expanded">▶</strong>
         </span>
 
-        <!--<span ng-if="!node.children || node.children.length === 0" style="width: 12px;"></span>-->
-
-        <!--<label>
-          <input type="checkbox" ng-model="node.checked" ng-change="toggleChildren(node)">
-          {{ node.title }}
-        </label>-->
-
         <div class="tree-node">
-
-          <span ng-if="node.type==='element'"
-            class="tree-node__label"
-            ng-click="displayAttributes(node)"
-            ng-class="{'tree-node__label--clickable': hasAttributes(node)}"
-          >{{ node.title }}</span>
+          <span ng-if="node.type === 'element'"
+                class="tree-node__label"
+                ng-click="displayAttributes(node)"
+                ng-class="{'tree-node__label--clickable': hasAttributes(node)}">
+            {{ node.title }}
+          </span>
         </div>
-
 
       </div>
 
-      <ul ng-if="node.children && node.children.length > 0 && node.expanded" style="margin-left: 20px;">
-        <li ng-repeat="child in node.children" ng-if="child.type=='element'">
-          <tree-node node="child"></tree-node>
+      <ul ng-if="node.children && node.children.length && node.expanded"
+          style="margin-left: 20px;">
+
+        <li ng-repeat="child in node.children track by child.path"
+            ng-if="child.type === 'element'">
+
+          <tree-node
+            node="child"
+            get-children-from-db="getChildrenFromDB"
+            display-attributes="displayAttributes"
+            config="config">
+          </tree-node>
+
         </li>
+
       </ul>
     `,
-    link: function(scope) {
-      scope.toggleChildren = scope.$parent.toggleChildren;
-      scope.getChildrenFromDB = scope.$parent.getChildrenFromDB;
-      scope.doSearch = scope.$parent.doSearch;
-      scope.config = scope.$parent.config;
-      scope.attributeList = scope.config.attributeList || [];
-      scope.displayAttributes = scope.$parent.displayAttributes;
-      scope.toggleExpand = function(node) {
+
+    link: function (scope) {
+
+      // Toggle expansion + lazy loading
+      scope.toggleExpand = function (node) {
         node.expanded = !node.expanded;
 
         if (node.expanded && (!node.children || !node.children.length)) {
@@ -265,20 +271,20 @@ app.directive('treeNode', function() {
         }
       };
 
-      scope.hasAttributes = function(node) {
-        if (!Array.isArray(scope.$parent.config.attributeList) || scope.$parent.config.attributeList.length === 0) {
+
+      scope.hasAttributes = function (node) {
+        if (!Array.isArray(scope.config.attributeList) ||
+          !scope.config.attributeList.length) {
           return false;
         }
 
-        return scope.$parent.config.attributeList.some(child => {
-          const expected = node.title + "|" + child.title;
-        return child.path.endsWith(expected);
+        return scope.config.attributeList.some(attr => {
+          const expected = node.title + "|" + attr.title;
+          return attr.path.endsWith(expected);
         });
       };
 
-      scope.isElement = function(child) {
-        return child.type === 'element';
-      }
     }
   };
 });
+
